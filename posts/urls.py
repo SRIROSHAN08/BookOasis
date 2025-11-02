0e7b288b9687b258d91906212cbfee77a707820e
Sriroshan M <sriroshanro8@gmail.com>
2025-11-02 23:05:30 +0530

    Weather added

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns=[
     path('book/',AllBooksView.as_view()),
     path('<int:pk>',BookDetailView.as_view(),name='book_detail'),
-    path('/weather/',WeatherView.as_view(),name='weather'),
+    path('weather/',WeatherView.as_view(),name='weather'),
 ]
